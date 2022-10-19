3be544b57f5fb199703b304d9bd501426a1f9219
leuleu420 <115033217+leuleu420@users.noreply.github.com>
2022-10-19 18:06:39 +0200

    cp

---
+++ TP1/API.js
@@ -0,0 +1,23 @@
+#API de tokenization
+
+const axios = require("axios");
+
+const encodedParams = new URLSearchParams();
+encodedParams.append("text", "my text payload");
+
+const options = {
+  method: 'POST',
+  url: 'https://linguist.p.rapidapi.com/v1/tokenizer',
+  headers: {
+    'content-type': 'application/x-www-form-urlencoded',
+    'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+    'X-RapidAPI-Host': 'linguist.p.rapidapi.com'
+  },
+  data: encodedParams
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
